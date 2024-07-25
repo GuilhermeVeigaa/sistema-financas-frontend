da4ea08e43c2535caf5320e3c5e73f418945ceca
guilhermeveigaa <guilhermeveigafff@gmail.com>
2024-07-25 13:36:58 -0300

    feat: add zod validation

--- src/components/ExpensesForm.tsx
+++ src/components/ExpensesForm.tsx
@@ -1,39 +1,60 @@
-import { useEffect, useRef, useState } from "react"
-import axios from "axios"
-import { authService } from "@/auth/authService"
+import { FormEvent, useEffect, useRef, useState } from "react";
+import { authService } from "@/auth/authService";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 export default function ExpensesForm({ onEdit, setOnEdit, getExpenses }:any) {
 
-    
+    const CreateExpensesSchema = z.object({
+      desc: z.string(),
+      value: z.coerce.number()
+    });
 
-    const [desc, setDesc] = useState('')
-    const [value, setValue] = useState('')
+    type CreateExpensesSchema = z.infer<typeof CreateExpensesSchema>
+
+    const { register, handleSubmit } = useForm<CreateExpensesSchema>({
+      resolver: zodResolver(CreateExpensesSchema),
+    });
+
+    function handleCreateExpenses(data: CreateExpensesSchema, e: any) {
+      e.preventDefault()
+
+      const desc = data.desc;
+      const value = data.value;
+
+      authService.addExpenses({
+        desc,
+        value,
+      })
+      .then( () => {
+        alert("Despesa/Receita criada com sucesso")
+        getExpenses()
+      })
+      .catch((error) => {
+        alert(error.message || "Erro ao criar despesa")
+        console.log(error)
+      })
+    };
 
   return (
-    <form onSubmit={(e) => {
-        e.preventDefault();
-
-        authService.addExpenses({
-          desc,
-          value,
-        }).then(() => {
-          alert("Receita cadastrada com sucesso")
-        }).catch((err) => {
-          console.error(err)
-          alert("erro ao cadastrar receita")
-        })
-      }} className="max-w-[1120px] bg-white m-5 w-[98%] py-4 gap-2 flex justify-around">
+    <form onSubmit={handleSubmit(handleCreateExpenses)} className="max-w-[1120px] bg-white m-5 w-[98%] py-4 gap-2 flex justify-around">
         <div className="flex flex-col">
             <span className="text-zinc-950 font-semibold">Descrição</span>
 
-            <input onChange={(e) => {setDesc(e.target.value)}} className="bg-white border border-zinc-950 rounded-md h-8 w-72 text-black" type="text" name="desc" />
+            <input
+             className="bg-white border border-zinc-950 rounded-md h-8 w-72 text-black"
+             type="text" 
+             { ...register('desc') } />
         </div>
 
         <div className='flex flex-col align-middle'>
-            <span className="text-zinc-950 font-semibold">
-                Valor
-            </span>
-            <input onChange={(e) => {setValue(e.target.value)}} className='bg-white border border-zinc-950 rounded-md h-8 w-72 text-black' type="number" name="value" />
+            <span className="text-zinc-950 font-semibold">Valor</span>
+
+            <input
+             className='bg-white border border-zinc-950 rounded-md h-8 w-72 text-black' 
+             type="number"
+             { ...register('value') } />
         </div>
 
         <div className='flex gap-3 pt-5'>
